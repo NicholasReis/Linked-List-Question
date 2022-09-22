ff98c86e00c10de8f640e4716df8b43a1297497b
Nicholas <46433505+NicholasReis@users.noreply.github.com>
2022-09-22 13:45:04 -0700

    Aligned it more closely to the java example

--- Linked List Python/linked_list.py
+++ Linked List Python/linked_list.py
@@ -4,8 +4,6 @@
 # Given the following code, write a solution to print out the values of the linked list in reverse order.
 # You are allowed to change any of the code we have provided if it will help in your solution.
 # You are allowed to use a search engine as long as you don't search for the solution.
-# There are some bugs intentionally left in that will not affect finding the solution.
-# Just fix them if you see them. :)
 
 class Node:
     """Class Node."""
@@ -43,33 +41,18 @@ class LinkedList:
         index_node = self.head
         while index_node.next.next is not None:
             index_node = index_node.next
+        popped_node = index_node.next
         index_node.next = None
+        return popped_node
 
     def size(self) -> str:
         """Returns the size of the linked list."""
         index_node = self.head
         count = 0
-        while index_node.next.next is not None:
+        while index_node is not None:
             count+=1
             index_node = index_node.next
         return count
-
-    def get_value_at_index(self, target_index) -> int:
-        """Returns the value at an index."""
-        index = 0
-        index_node = self.head
-        if index < target_index:
-            index_node = index_node.next
-        return index_node.data
-
-    def get_index_of_value(self, target_data) -> int:
-        """Returns the index of the input data."""
-        index = 0
-        index_node = self.head
-        while index_node.data != target_data:
-            index+=1
-            index_node = index_node.next
-        return index
 
     def print(self):
         """Prints the linked list."""
